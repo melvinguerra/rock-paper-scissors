22fe44977211d98595527ddd106ecc5704818b32
Melvin Guerra <26179386+melvinguerra@users.noreply.github.com>
2024-12-10 16:30:58 +0800

    Add playRound logic
    Add score to computerScore if computer wins and add score to humanScore if human wins

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let humanScore = 0;
 
 function getComputerChoice(){
     let rand = Math.floor(Math.random() * 100) // Generate random number between 0 and less than 1 then multiply by 100 to get an integer value then use Math.floor to return the value without decimal values
+// <= 33 = rock (0-33)
+// <= 66 = paper (34-66)
+// < 100 = scissors ( 67-100)
     return rand;
 }
 
@@ -13,8 +16,40 @@ function getHumanChoice(){
 
 function playRound(humanChoice, computerChoice){
     humanChoice = humanChoice.toLowerCase();
+    if (computerChoice <= 33){
+        computerChoice = "rock";
+    } else if( computerChoice <= 66){
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+
     if(humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors"){
         alert("Invalid answer");
+    } else if(humanChoice === "rock" && computerChoice == "rock") {
+        alert(`It's a tie! You both chose ${humanChoice}`);
+    } else if (humanChoice === "rock" && computerChoice === "paper"){
+        alert(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    } else if (humanChoice === "rock" && computerChoice === "scissors"){
+        alert(`You won! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else if(humanChoice === "paper" && computerChoice == "rock") {
+        alert(`You won! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else if(humanChoice === "paper" && computerChoice == "paper") {
+        alert(`It's a tie! You both chose ${humanChoice}`);
+    } else if(humanChoice === "paper" && computerChoice == "scissors") {
+        alert(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    } else if(humanChoice === "scissors" && computerChoice == "rock") {
+        alert(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    } else if(humanChoice === "scissors" && computerChoice == "paper") {
+        alert(`You won! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    } else if(humanChoice === "scissors" && computerChoice == "scissors") {
+        alert(`It's a tie! You both chose ${humanChoice}`);
     }
 }
 
