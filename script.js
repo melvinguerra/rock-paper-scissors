c104f7fe838a92bcc8571d2bb13df3098195f2eb
Melvin Guerra <26179386+melvinguerra@users.noreply.github.com>
2024-12-10 15:16:01 +0800

    Add variables to keep track of scores
    Create computerScore and humanScore variables to keep track of scores
    and initialized with value of 0

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let computerChoice = "";
 let humanChoice = "";
+let computerScore = 0;
+let humanScore = 0;
 
 function getComputerChoice(){
     let rand = Math.floor(Math.random() * 100) // Generate random number between 0 and less than 1 then multiply by 100 to get an integer value then use Math.floor to return the value without decimal values
