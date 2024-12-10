ed25c10d9840aa44959d7ae9fe6895a12dc09554
Melvin Guerra <26179386+melvinguerra@users.noreply.github.com>
2024-12-10 15:13:37 +0800

    Add function getHumanChoice to accept input
    Create function to accept user input. If user's input is not
    "Rock", "Paper", or "Scissors", it will alert "Invalid input"

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let computerChoice = "";
 let humanChoice = "";
 
 function getComputerChoice(){
-    let rand = Math.floor(Math.random() * 100) // Generate random number between 0 and less than 1 then multiply by 100 to get an integer value then use Math.floor to return the value without decimal
+    let rand = Math.floor(Math.random() * 100) // Generate random number between 0 and less than 1 then multiply by 100 to get an integer value then use Math.floor to return the value without decimal values
     if (rand <= 33){
         computerChoice = "Rock";
     } else if( rand <= 66){
@@ -12,3 +12,9 @@ function getComputerChoice(){
     }
 }
 
+function getHumanChoice(){
+    humanChoice = prompt("Type Rock, Paper, Scissors");
+    if(humanChoice !== "Rock" || humanChoice !== "Paper" || humanChoice !== "Scissors") {
+        alert("Invalid answer");
+    }
+}
