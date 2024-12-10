5122ad2fe1420340220def31286ba3baaaa9c9a9
Melvin Guerra <26179386+melvinguerra@users.noreply.github.com>
2024-12-10 16:10:33 +0800

    Update getHumanChoice logic and playRound logic
    - Moved validation of user input to playRound logic
    - Removed not used global variables
    -

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-let computerChoice = "";
-let humanChoice = "";
 let computerScore = 0;
 let humanScore = 0;
 
 function getComputerChoice(){
     let rand = Math.floor(Math.random() * 100) // Generate random number between 0 and less than 1 then multiply by 100 to get an integer value then use Math.floor to return the value without decimal values
-    if (rand <= 33){
-        computerChoice = "Rock";
-    } else if( rand <= 66){
-        computerChoice = "Paper";
-    } else {
-        computerChoice = "Scissors";
-    }
+    return rand;
 }
 
 function getHumanChoice(){
-    humanChoice = prompt("Type Rock, Paper, Scissors");
-    humanChoice.toLowerCase();
-    if(humanChoice !== "rock" || humanChoice !== "paper" || humanChoice !== "scissors") {
-        alert("Invalid answer");
-    }
+    let humanChoice = prompt("Type Rock, Paper, Scissors");
+    return humanChoice;
 }
 
 function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    if(humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors"){
+        alert("Invalid answer");
+    }
 }
 
 const humanSelection = getHumanChoice();
